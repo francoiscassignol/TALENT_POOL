de63cbef8a19445aea06ae52f1cdef331f8a9265
aude idhammouaubert <aude.idh@gmail.com>
2018-08-14 13:39:27 -0400

    Controller / view / model job offer

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,3 +1,7 @@
 class JobOffer < ApplicationRecord
   belongs_to :user_id
+  CATEGORIES = ["Marketing", "Finance", "Executive", "Tech", "HR", "Sales", "Legal"]
+  DIVISIONS = ["M. Bricolage", "Intersport"]
+  CONTRACT_TYPES = ["Full time", "Part time", "Intern"]
+  LOCATIONS = ["Pamiers", "Toulouse Fenouillet", "Montauban", "Castelsarasin", "Castres", "Bias"]
 end
