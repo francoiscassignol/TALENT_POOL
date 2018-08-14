d657ede19c8c6c6f24d17680fcbd8bcb91384e3d
francois cassignol <cassignol.fr@gmail.com>
2018-08-14 17:54:01 -0400

    corrected some bugs all together

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'job_offers#index'
   resources :job_offers do
     resources :applications, only: [:index, :new, :create]
   end
   resources :applications, except: [:index, :new, :create]
-  resources :accounts, only: [:new, :create, :edit, :update]
 
   get 'applications', to: "applications#all", as: "all_applications"
 end
