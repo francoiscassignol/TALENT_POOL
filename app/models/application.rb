f8962d5980230ace0d83b90e25f60f36266f9fe6
aude idhammouaubert <aude.idh@gmail.com>
2018-08-15 16:09:51 -0400

    fetch forms

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,6 @@ class Application < ApplicationRecord
   STATUS = ["Under review", "In process", "Rejected", "Accepted"]
   COUNTRY = %w(France Andorre Espagne)
   #validates
+  mount_uploader :profile_picture, PhotoUploader
+  mount_uploader :resume, ResumeUploader
 end
