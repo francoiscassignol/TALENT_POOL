14b8843ce896133b8dadfad794947fc666c61a8d
francois cassignol <cassignol.fr@gmail.com>
2018-08-17 10:28:21 -0400

    reglex fix email

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,4 +7,10 @@ class Application < ApplicationRecord
   #validates
   mount_uploader :profile_picture, PhotoUploader
   mount_uploader :resume, ResumeUploader
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "please type a valid email" }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :cover_letter, length: { minimum: 30 }
+  validates :phone_number, numericality: { only_integer: true }
+  validates :post_code, numericality: { only_integer: true }
 end
